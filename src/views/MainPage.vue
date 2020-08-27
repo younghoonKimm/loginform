532e705fbc404a46a64667a83754d3b6f0178083
kimyounghoon <choosey_@naver.com>
2020-08-27 15:10:52 +0900

    [#3] ADD Main page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
-    Today I Learned
+    <div class="main list-container contents">
+      <h1 class="page-header">Today I Learned</h1>
+      <ul>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index';
+import PostListItem from '@/components/posts/PostListItem';
+
+export default {
+  data() {
+    return {
+      postItems: [],
+    };
+  },
+  components: {
+    PostListItem,
+  },
+  methods: {
+    async fetchData() {
+      const { data } = await fetchPosts();
+      this.postItems = data.posts;
+      console.log(this.postItems);
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
